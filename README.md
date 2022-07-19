ac9a0e6ce21f5aa90e02c7c0ec3db1db79f23048
nunixnunix04 <nunixnunix@gmail.com>
2022-07-19 17:15:54 -0400

    added .R script for extracting BOLT-REML results for XWAS diseases and exposures

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PXS Pipeline
 
 ## Input files to edit:
-- `phenotypes.txt`: on each line ,write the name of the phenotypes (according to the coeffs and tally .csv) that an analysis should be performed on
+- `phenotypes.txt`: on each line, write the name of the phenotypes (according to the coeffs and tally .csv) that an analysis should be performed on
 - `CRFs_table.txt`: table of clinical risk factors for each phenotype
 
 ## Order of scripts to run:
@@ -9,3 +9,4 @@
 2. `run_BOLT_PXS.sh`: runs BOLT-LMM and BOLT-REML on the PXSs of the phenotypes
 3. `run_BOLT_exposures.sh`: runs BOLT-LMM and BOLT-REML on the exposures of the PXS
 4. `run_genCorr_CRFs.sh`: run BOLT-REML (genetic correlation) for each PXS and its disease's clinical risk factors
+5. `aggregate_results.R`: extracts important information from BOLT-REML results for XWAS diseases and exposures
---
+++ aggregate_results.R
@@ -0,0 +1,101 @@
+## Libraries and directories ##
+library(tidyverse)
+library(data.table)
+
+dir_scratch <- "~/scratch3/PXS_pipeline/"
+dir_script <- "~/jobs/PXS_pipeline/"
+dir_data_showcase <- "~/scratch3/key_data/"
+
+## Functions ##
+extract_from_REML <- function(REML) {
+  N <- length(REML)
+  
+  # extract time for analysis
+  line <- REML[N]
+  elapsed_hours <- as.numeric(str_split(line," ")[[1]][7]) / (60 * 60)
+  
+  # extract h2e
+  line <- REML[N-4]
+  line_split <- str_split(line, ": ")[[1]][2]
+  h2e <- as.numeric(str_split(line_split, " ")[[1]][1])
+  h2e_err <- parse_number(str_split(line_split, " ")[[1]][2])
+  
+  # extract h2g
+  line <- REML[N-2]
+  line_split <- str_split(line, ": ")[[1]][2]
+  h2g <- as.numeric(str_split(line_split, " ")[[1]][1])
+  h2g_err <- parse_number(str_split(line_split, " ")[[1]][2])
+  
+  out <- c(h2e, h2e_err, h2g, h2g_err, elapsed_hours)
+  out
+}
+
+## Code ##
+
+### REML results for each phenotypes
+loc_phenolist <- paste0(dir_script,"phenotypes_ALL.txt")
+pheno_list <- readLines(loc_phenolist)
+
+REML_pheno_tbl <- tibble(
+  field = as.character(),
+  h2e = as.numeric(),
+  h2e_err = as.numeric(),
+  h2g = as.numeric(),
+  h2g_err = as.numeric(),
+  elapsed_hours = as.numeric()
+)
+REML_expo_tbl <- REML_pheno_tbl
+for (pheno in pheno_list) {
+  loc_REML <- paste0(dir_scratch,pheno,"/",pheno,"_PXS_BOLTREML.out")
+  REML <- readLines(loc_REML)
+  out <- extract_from_REML(REML)
+  
+  # adds row to table
+  REML_pheno_tbl <- REML_pheno_tbl %>%
+    add_row(
+      field = pheno,
+      h2e = out[1],
+      h2e_err = out[2],
+      h2g = out[3],
+      h2g_err = out[4],
+      elapsed_hours = out[5]
+    )
+  
+  print(paste("Read REML results for",pheno))
+}
+
+### REML results for each exposure
+loc_expolist <- paste0(dir_script,"exposures.txt")
+exposures_list <- readLines(loc_expolist)
+
+for (expo in exposures_list) {
+  loc_REML <- paste0(dir_scratch,"exposures/",expo,"/",expo,"_BOLTREML.out")
+  if (!file.exists(loc_REML)) {next}
+  REML <- readLines(loc_REML)
+  out <- extract_from_REML(REML)
+  
+  # adds row to table
+  REML_expo_tbl <- REML_expo_tbl %>%
+    add_row(
+      field = expo,
+      h2e = out[1],
+      h2e_err = out[2],
+      h2g = out[3],
+      h2g_err = out[4],
+      elapsed_hours = out[5]
+    )
+  
+  print(paste("Read REML results for",expo))
+}
+
+### Appends fields name to REML tables
+ukb_dict <- as_tibble(fread(paste0(dir_data_showcase,"Data_Dictionary_Showcase.tsv"))) %>%
+  mutate(field = paste0("f.",FieldID,".0.0")) %>%
+  select(field, fieldname=Field) %>%
+  add_row(
+    field = c("AF", "CAD", "COPD", "T2D","fev1_inst1"),
+    fieldname = c("Atrial fibrillation", "Coronary artery disease", "Chronic obstructive pulmonary disease",
+                  "Type 2 diabetes", "Forced expiratory volume in 1-second (FEV1)")
+  )
+REML_pheno_tbl <- REML_pheno_tbl %>% left_join(ukb_dict, by="field")
+REML_expo_tbl <- REML_expo_tbl %>% left_join(ukb_dict, by="field")
