d6902c32c79a98bb7beca68c02ad80a918f2ef1f
nunixnunix04 <nunixnunix@gmail.com>
2022-07-19 14:31:55 -0400

    Extract CRFs for T2D during initial_setup.R

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PXS Pipeline
+
+## Order of scripts to run/edit:
+0. `phenotypes.txt`: on each line ,write the name of the phenotypes (according to the coeffs and tally .csv) that an analysis should be performed on
+0. `CRFs.txt`: table of clinical risk factors for each phenotype
+1. `initial_setup.sh`: extracts necessary fields from phenotype file, and initiates scripts that compute the PXS for each phenotype
+2. `run_BOLT_PXS.sh`: runs BOLT-LMM and BOLT-REML on the PXSs of the phenotypes
+3. `run_BOLT_exposures.sh`: runs BOLT-LMM and BOLT-REML on the exposures of the PXS
--- initial_setup.R
+++ initial_setup.R
@@ -6,21 +6,24 @@ dir_out <- "~/scratch3/PXS_pipeline/"
 loc_coeffs <- "/n/groups/patel/yixuan/PXS_multi/all/coeffs_V9.csv"
 loc_tally <- "/n/groups/patel/yixuan/PXS_multi/all/tally_X_V9.csv"
 loc_pheno_full <- "/n/groups/patel/uk_biobank/main_data_9512/ukb9512.tab"
+loc_pheno2_full <- "/n/groups/patel/uk_biobank/main_data_32144/ukb32144.csv"
 loc_40PCs <- "~/scratch3/key_data/UKB_40PCs_500k.txt"
+loc_CFRs_tbl <- "~/jobs/PXS_pipeline/CRFs_table.txt"
 
-# List of exposures to remove from data for any reason
+# List of exposures to remove from data for any reason, as well as covariates
 remove_exposures <- c("f.20118.0.0")
 covars <- c("f.31.0.0", # sex
             "f.34.0.0", # year of birth
             "f.54.0.0", # assessment center
             paste0("pc",1:40) # 40 PCs not from UKB field directly
             )
-
 ## Code
 
 # loads coefficients and tally file from XWAS results
 coeffs <- as_tibble(fread(loc_coeffs))
 tally <- as_tibble(fread(loc_tally))
+# loads list of CRFs
+CRFs <- (as_tibble(fread(loc_CFRs_tbl)))$field
 
 categorical_vars <- coeffs %>% select(X) %>%
   filter(X %in% tally$X) %>%
@@ -30,16 +33,17 @@ categorical_vars <- coeffs %>% select(X) %>%
     X = as.numeric(X)
   )
 
-# create emtoy fields tibble
+# create empty fields tibble
 fields <- tibble(
   code = tally$X,
   field = paste0("f.",code,".0.0"),
   use_type = "exposure"
 )
 
-# Adds the covariates, exposures, and IID fields to the fields tibble
+# Adds the covariates, exposures, CFRs, and IID fields to the fields tibble
 fields <- fields %>%
   add_row(code=NA,field=covars,use_type="covar") %>%
+  add_row(code=NA,field=CRFs,use_type="CRF") %>%
   left_join(tally, by=c("code"="X")) %>%
   add_row(code = NA, field=c("f.eid","FID","IID"), use_type="identification",category="", fieldname="") %>%
   left_join(categorical_vars, by=c("code"="X")) %>%
@@ -50,11 +54,26 @@ fields <- fields %>%
 
 PCs40 <- as_tibble(fread(loc_40PCs)) %>% select(-FID)
 pheno <- as_tibble(fread(loc_pheno_full, select=fields$field))
+remaining_fields <- fields$field[!((fields$field) %in% colnames(pheno))]
+pheno2 <- as_tibble(fread(loc_pheno2_full, select=c("eid",remaining_fields)))
+# corrects field names to match others in pheno table and fields table
+for (i in 2:length(colnames(pheno2))) {
+  oldfield <- colnames(pheno2[i])
+  fieldnum <- str_split(oldfield,"-")[[1]][1]
+  fieldinst <- str_split(oldfield,"-")[[1]][2]
+  field <- paste0("f.",fieldnum,".",fieldinst)
+  colnames(pheno2)[i] <- field
+  
+  fields[fields$field==oldfield,"field"] <- field
+}
 pheno <- pheno %>%
   select(FID=f.eid, IID=f.eid,all_of(colnames(pheno[2:length(colnames(pheno))]))) %>%
   arrange(IID) %>%
-  left_join(PCs40, by="IID")
-
+  left_join(PCs40, by="IID") %>%
+  left_join(pheno2, by=c("IID"="eid"))
+  
+# calculates the percentage of NA results per field and then removes fields with
+# 100% missingness rate
 for (i in 1:nrow(fields)) {
   field <- fields$field[i]
   
