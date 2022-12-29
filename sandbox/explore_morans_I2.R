39eb6c6e13222f709e843f03e3d57d3907f78bd6
nunixnunix04 <nunixnunix@gmail.com>
2022-12-29 14:38:50 -0500

    cleaned up code for plotting geospatial clustering of traits, including Moran's I

--- sandbox/explore_morans_I2.R
+++ sandbox/explore_morans_I2.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(data.table)
 library(sf)
 library(spdep)
-library(ggrepel)
 
 dir_script <- "~/jobs/PXS_pipeline/code/"
 dir_scratch <- "~/scratch3/PXS_pipeline/"
@@ -11,7 +10,7 @@ loc_fields <- paste0(dir_scratch,"fields_tbl.txt")
 # phenotype file containing home and birthplace coordinates
 loc_pheno_full2 <- "/n/groups/patel/uk_biobank/project_22881_669542/ukb669542.csv"
 ## loading shape file
-#loc_shp_MSOA <- "~/scratch3/key_data/infuse_MSOA_2011/infuse_msoa_lyr_2011_clipped.shp"
+loc_shp_MSOA <- "~/scratch3/key_data/infuse_MSOA_2011/infuse_msoa_lyr_2011_clipped.shp"
 loc_shp_LA <- "~/scratch3/key_data/infuse_LA_2011/infuse_dist_lyr_2011_clipped.shp"
 s <- sf::st_read(loc_shp_LA)
 
@@ -48,12 +47,14 @@ s$region_index <- 1:nrow(s)
 s$n_home_pop <- rowSums(out_home)
 s$n_birth_pop <- rowSums(out_birth)
 NI_indices <- s[!(substring(s$geo_code,1,1) %in% c("E","W","S")),]$region_index
+#s <- s[-NI_indices,]
 pheno <- pheno %>% filter(!(region_home_index %in% NI_indices),
                           !(region_birth_index %in% NI_indices))
 
 # gets mean exposure value for each region
 fields_tbl <- as_tibble(fread(loc_fields))
-expos <- c(fields_tbl$term[fields_tbl$use_type=="exposure"],"PXS_T2D")
+expos <- fields_tbl$term[fields_tbl$use_type=="exposure"]
+#expos <- fields_tbl$term
 
 for (i in s$region_index) {
   colmeans_home <- colMeans(pheno[pheno$region_home_index == i,expos], na.rm=TRUE)
@@ -100,60 +101,71 @@ for (expo in expos) {
   
   print(expo)
 }
-morans_table <- tibble(term = expos) %>%
-  left_join(fields_tbl %>% select(term, fieldname, Meaning), by="term") %>%
-  mutate(
-    home_I = home_Is, home_p = p.adjust(home_ps,method="fdr"),
-    birth_I = birth_Is, birth_p = p.adjust(birth_ps,method="fdr"))
 
-# function for plotting a trait
-plot_geospatial <- function(shapefile, trait_col, trait_name, I, p, coords) {
-  title <- paste0("Geospatial clustering of '",trait_name,"'\nbased on ", coords, " coordinates")
-  subtitle <- paste0("Moran's I = ", round(I,3), " :: adjusted p-value = ", formatC(p, format="E", digits=2))
-  
-  
-  ggplot(shapefile) +
-    geom_sf(aes(fill=!!sym(trait_col)), size=0.2) +
-    scale_fill_gradient2(low="green",mid="white", high="red",
-                         midpoint = median(shapefile[[trait_col]])) +
-    labs(title = title, subtitle = subtitle, fill = "Trait Value")
-}
-dir.create(paste0(dir_scratch,"figures/birth_I"), recursive=TRUE)
-dir.create(paste0(dir_scratch,"figures/home_I"), recursive=TRUE)
-# loops through each trait and birth/home and saves geospatial plot of exposure values
-for (coords in c("birth", "home")) {
-  for (i in 1:nrow(morans_table)) {
-    if (coords=="birth") {
-      shapefile <- s_birth[-NI_indices,]
-      I <- morans_table$birth_I[i]
-      p <- morans_table$birth_p[i]
-      dir_out <- paste0(dir_scratch,"figures/birth_I/")
-    } else if (coords=="home") {
-      shapefile <- s_home[-NI_indices,]
-      I <- morans_table$home_I[i]
-      p <- morans_table$home_p[i]
-      dir_out <- paste0(dir_scratch,"figures/home_I/")
-    }
-    
-    trait_col <- morans_table$term[i]
-    
-    if (is.na(morans_table$Meaning[i])) {
-      if (is.na(morans_table$fieldname[i])) {
-        trait_name <- "PXS for Type II Diabetes"
-      } else {trait_name <- morans_table$fieldname[i]}
-    } else {
-      trait_name <- paste0(morans_table$fieldname[i],": ", morans_table$Meaning[i])
-    }
-    
-    gg <- plot_geospatial(shapefile = shapefile,
-                    trait_col = trait_col, trait_name = trait_name,
-                    I = I, p = p, coords = coords)
-    
-    loc_out <- paste0(dir_scratch,"figures/",coords,"_I/",trait_col,"_",coords,"_geospatial.png")
-    ggsave(loc_out, plot=gg, width = 2000, height = 3000, units = "px")
-    print(paste("Saved plot for", coords,"Moran's I for", trait_col))
-  }
-}
+fields_tbl$home_I[fields_tbl$use_type=="exposure"] <- home_Is
+#fields_tbl$home_I <- home_Is
+fields_tbl$home_p[fields_tbl$use_type=="exposure"] <- p.adjust(home_ps,method="fdr")
+#fields_tbl$home_p <- home_ps
+
+#fields_tbl$birth_I <- birth_Is
+fields_tbl$birth_I[fields_tbl$use_type=="exposure"] <- birth_Is
+#fields_tbl$birth_p <- birth_ps
+fields_tbl$birth_p[fields_tbl$use_type=="exposure"] <- p.adjust(birth_ps,method="fdr")
+
+fields_tbl$delta_I <- fields_tbl$home_I - fields_tbl$birth_I
+
+fields_tbl %>% arrange(birth_p)
+fields_tbl %>% arrange(-abs(delta_I))
+
+library(ggrepel)
+ggplot(fields_tbl, aes(x=birth_I,y=home_I)) +
+  geom_abline(slope=1) +
+  geom_smooth(method="lm") +
+  geom_point() +
+  geom_label_repel(data=fields_tbl %>% filter(home_I>birth_I), aes(label=fieldname))
+
+# ggplot(s_birth[-NI_indices]) +
+#   geom_sf(aes(fill=pc11), size=0.2) +
+#   scale_fill_gradient(low="red",high="white") #scale_fill_brewer(palette = "YlOrRd")
+ggplot(s) +
+  geom_sf(aes(fill=f728), size=0.2) +
+  labs(title="Spatial distribution of 'Number of vehicles in household'",
+       substitle=paste0("Values are inverse-ranked transformed. I = ",fields_tbl$home_I[fields_tbl$term == "f728"]))
 
 
+### f728 - number of vehicles in the household ###
+s_f728 <- tibble(region_index = 1:nrow(s), f728 = s_home$f728)
+#f728_home = s_home$f728,
+#f728_birth = s_birth$f728)
+movers <- pheno %>% filter(region_birth_index != region_home_index) %>%
+  select(FID, IID, region_home_index, region_birth_index) %>%
+  left_join(s_f728 %>% rename("home_f728"="f728"), by=c("region_home_index"="region_index")) %>%
+  left_join(s_f728 %>% rename("birth_f728"="f728"), by=c("region_birth_index"="region_index")) %>%
+  mutate(delta_f728 = home_f728 - birth_f728)
 
+t1 <- t.test(movers$delta_f728)
+ggplot(movers, aes(x=delta_f728)) +
+  geom_histogram(binwidth = 0.05) +
+  xlim(-1,1) +
+  geom_vline(xintercept = 0) +
+  #geom_rect(ymin=-Inf,ymax=Inf, xmin=t1$conf.int[1], xmax=t1$conf.int[2], fill="red", alpha=0.5) +
+  geom_vline(xintercept = t1$estimate, color="red")
+
+
+### f1160 - sleep duration ###
+s_f1160 <- tibble(region_index = 1:nrow(s), f1160 = s_home$f1160)
+#f1160_home = s_home$f1160,
+#f1160_birth = s_birth$f1160)
+movers <- pheno %>% filter(region_birth_index != region_home_index) %>%
+  select(FID, IID, region_home_index, region_birth_index) %>%
+  left_join(s_f1160 %>% rename("home_f1160"="f1160"), by=c("region_home_index"="region_index")) %>%
+  left_join(s_f1160 %>% rename("birth_f1160"="f1160"), by=c("region_birth_index"="region_index")) %>%
+  mutate(delta_f1160 = home_f1160 - birth_f1160)
+
+t1 <- t.test(movers$delta_f1160)
+ggplot(movers, aes(x=delta_f1160)) +
+  geom_histogram(binwidth = 0.05) +
+  xlim(-1,1) +
+  geom_vline(xintercept = 0) +
+  #geom_rect(ymin=-Inf,ymax=Inf, xmin=t1$conf.int[1], xmax=t1$conf.int[2], fill="red", alpha=0.5) +
+  geom_vline(xintercept = t1$estimate, color="red")
