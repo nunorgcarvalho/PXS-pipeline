009342a564b22d9b863b889509cfad6bce1680cb
nunixnunix04 <nunixnunix@gmail.com>
2023-05-23 11:16:29 -0400

    fixed PXS coefficient printing code

--- sandbox/print_simple_PXS_coefficients.R
+++ sandbox/print_simple_PXS_coefficients.R
@@ -1,8 +1,14 @@
-coeffs_raw <- as_tibble(fread("~/jobs/PXS_pipeline/input_data/PXS_coefficients.txt"))
-coeffs <- coeffs_raw %>% rowwise() %>%
-  mutate(Field_Name = paste0(ifelse(is.na(fieldname),term,fieldname),
-                             ifelse(is.na(Meaning),"",paste0(": ", Meaning)))) %>%
+library(tidyverse)
+library(data.table)
+coeffs_raw <- as_tibble(fread("../input_data/PXS_coefficients.txt"))
+fields_tbl <- as_tibble(fread("../scratch/fields_tbl.txt"))
+expos <- (fields_tbl %>% filter(use_type=="exposure"))$term
+coeffs <- coeffs_raw %>% 
+  filter(term %in% expos) %>% rowwise() %>%
+  mutate(Field_Name = paste0(ifelse(fieldname=="",term,fieldname),
+                             ifelse(Meaning=="","",paste0(": ", Meaning)))) %>%
   select(Field_Name, p.value, estimate) %>%
   filter(substring(Field_Name,1,2) != "pc")
 
-  
+loc_out <- "../sandbox/scratch/PXS_T2D_coeffs_tbl_simple.txt"
+fwrite(coeffs, loc_out, sep="\t")  
