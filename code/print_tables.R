23067fed45669a32637e7d4a6b2c179592de1c66
nunixnunix04 <nunixnunix@gmail.com>
2023-06-22 19:26:45 -0400

    added code for outputting fields + coeffs tables

--- code/print_tables.R
+++ code/print_tables.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(data.table)
 library(gt)
+source('paths.R')
 
 dir_results <- paste0(dir_script,"../final_results/")
 
@@ -14,9 +15,13 @@ gt_theme <- function(data) {
     cols_align(
       align = "center",
       columns = everything()
+    ) %>%
+    tab_style(
+      style = cell_text(weight = "bold"),
+      locations = cells_column_labels(columns = everything())
     )
 }
-rounding_decimals <- 3
+rnd_dec <- 3
 
 # GWAS Catalog Table ####
 ## Trait count ####
@@ -50,3 +55,78 @@ tbl_gt <- gt(tbl) %>%
   )
 tbl_gt
 gtsave(tbl_gt, "GWAS_catalog_group_count.png",dir_results)
+
+# Fields Table ####
+## All behavioral traits ####
+loc_tbl <- paste0(dir_script,"../input_data/T2D_XWAS_exposures.txt")
+tbl_raw <- as_tibble(fread(loc_tbl))
+ukb_dict <- as_tibble(fread(paste0(dir_data_showcase,"Data_Dictionary_Showcase.tsv")))
+
+tbl <- tbl_raw %>% filter(Exposure_Class=="agency") %>%
+  arrange(FieldID) %>%
+  left_join(ukb_dict[,c("FieldID","ValueType")], by="FieldID") %>%
+  select(FieldID, Field, ValueType)
+
+tbl_gt <- gt(tbl) %>%
+  gt_theme() %>%
+  cols_label(
+    FieldID = "UKBB Field ID",
+    Field = "Field Name",
+    ValueType = "Data Type") %>%
+  cols_align(align = "left", columns = c("Field")) %>%
+  tab_style(
+    style = cell_text(weight = "bold"),
+    locations = cells_column_labels(columns = everything())
+  )
+tbl_gt
+gtsave(tbl_gt, "fields_ALL_behaviors.png",dir_results) # maybe just save as .csv?
+
+## PXS Coefficients ####
+loc_tbl <- paste0(dir_script,"../input_data/PXS_coefficients.txt")
+tbl_raw <- as_tibble(fread(loc_tbl))
+shortnames <- as_tibble(fread(paste0(dir_script,"../input_data/exposures_shortnames.csv")))
+fields <- as_tibble(fread(paste0(dir_scratch,"fields_tbl.txt")))
+
+# sets maps for renaming covariates and use types
+map_shortname <- setNames(c("Sex","Age",paste0("PC",1:40)),
+                          c("sex","age",paste0("pc",1:40)) )
+map_usetype <- setNames(c("Covariate", "Behavior"),
+                        c("covar","exposure") )
+
+# joins coefficients, shortnames, and field usetype
+tbl <- tbl_raw %>%
+  left_join(shortnames, by="term") %>%
+  left_join(fields[c("term","use_type")], by="term") %>%
+  mutate(shortname = ifelse(is.na(shortname),term,shortname)) %>%
+  mutate(shortname = recode(shortname, !!!map_shortname),
+         use_type = recode(use_type, !!!map_usetype)) %>%
+  select(use_type, fieldID, value, shortname, estimate, p.value)
+# manually sets field ID for sex and age
+tbl$fieldID[tbl$shortname=="Sex"] <- 31
+tbl$fieldID[tbl$shortname=="Age"] <- 34
+# resorts table to first be exposures (sorted by p-value) and then covariates
+tbl <-  tbl %>% filter(use_type=="Behavior") %>% arrange(p.value) %>%
+  add_row(tbl %>%
+            filter(use_type=="Covariate") %>%
+            mutate(shortname = factor(shortname, levels = unname(map_shortname))) %>%
+            arrange(shortname)
+          )
+
+tbl_gt <- gt(tbl) %>%
+  gt_theme() %>%
+  cols_label(
+    use_type = "Variable Type",
+    fieldID = "UKBB Field ID",
+    value = "UKBB Value",
+    shortname = "Variable Name",
+    estimate = "Standardized Coefficient",
+    p.value = "XWAS p-value"
+  ) %>%
+  fmt_number(columns = c("estimate"), decimals = 5) %>%
+  fmt_scientific(columns = "p.value") %>%
+  sub_missing(missing_text = "") %>%
+  cols_width(fieldID ~ px(80),
+             value ~ px(60),
+             estimate ~ px(100))
+tbl_gt
+gtsave(tbl_gt, "PXS_coefficients.png",dir_results) # maybe just save as .csv?
