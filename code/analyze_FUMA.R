dfe2202b945f8f3fef0f7c80f3b804d675e786c1
nunixnunix04 <nunixnunix@gmail.com>
2023-06-22 15:09:49 -0400

    removed code for PXS-pvalue vs GTEx-p.value analysis

--- code/analyze_FUMA.R
+++ code/analyze_FUMA.R
@@ -1,4 +1,4 @@
-#### Libraries and paths ####
+# Libraries and paths ####
 library(tidyverse)
 library(data.table)
 library(ggrepel)
@@ -13,7 +13,7 @@ jobID_tbl <- as_tibble(fread(paste0(dir_FUMA,"jobid_name.csv"))) %>%
   select(-V4)
 jobID_tbl[jobID_tbl$JobName=="PXS NC",c("term","traitname")] <- list("PXS_T2D","PXS for Type 2 Diabetes onset")
 
-#### GTEx Data ####
+# GTEx Data ####
 # gets order of exposures from decreasing p-value
 coeffs <- as_tibble(fread(paste0(dir_script,"../input_data/PXS_coefficients.txt")))
 expo_order <- factor(c("PXS for Type 2 Diabetes onset",
@@ -22,7 +22,7 @@ expo_order <- factor(c("PXS for Type 2 Diabetes onset",
                           left_join(fields%>%select(term,traitname), by="term") %>%
                           arrange(p.value))$traitname) )
 
-# looks at GTEx general data
+## GTEx Category data ####
 for (i in 1:(nrow(jobID_tbl))) {
   jobID <- jobID_tbl$JobID[i]
   term <- jobID_tbl$term[i]
@@ -42,7 +42,7 @@ for (i in 1:(nrow(jobID_tbl))) {
 GTEx_general$P_adjusted <- p.adjust(GTEx_general$P / length(unique(GTEx_general$VARIABLE)), method = 'bonferroni')
 GTEx_general$significant_P_adj <- GTEx_general$P_adjusted < 0.05
 GTEx_general %>% filter(significant_P_adj) %>% arrange(P_adjusted) %>% print(n=Inf)
-# tile plot
+### tile plot ####
 ggplot(GTEx_general, aes(x = VARIABLE,
                          y = fct_rev(factor(traitname, levels=expo_order)))) +
   geom_tile(aes(fill=-log10(P_adjusted))) +
@@ -51,69 +51,13 @@ ggplot(GTEx_general, aes(x = VARIABLE,
             color = "black", fill = NA) +
   scale_x_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width=30), expand = c(0, 0)) +
   scale_y_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width=50), expand = c(0, 0)) +
-  scale_fill_gradient2(low="white",mid="white", high="green", midpoint = -log10(0.05)) +
+  scale_fill_gradient2(low="red",mid="white", high="green", midpoint = -log10(0.05)) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   xlab("Tissue Category") + ylab("Trait") +
   labs(title = "Average gene expression per tissue category for each behavior",
        subtitle = "Using MAGMA with GTEx through FUMA. White: p<0.05 (Bonferroni adjusted)")
 
-# compares brain expression to T2D_onset association
-Brain_PXS_tbl <- GTEx_general %>%
-  filter(VARIABLE=="Brain", term != "PXS_T2D") %>%
-  left_join(coeffs %>% select(term, estimate, p.value), by="term") %>%
-  rename(Brain_P = P_adjusted, PXS_P = p.value) #%>% filter(-log10(PXS_P) < 20)
-(cor1 <- cor.test(rank(Brain_PXS_tbl$Brain_P), rank(Brain_PXS_tbl$PXS_P)))
-ggplot(Brain_PXS_tbl, aes(x = rank(Brain_P), y = rank(PXS_P))) +
-  geom_smooth(method='lm') +
-  geom_point() +
-  geom_text_repel(aes(label = traitname)) +
-  scale_x_reverse() +
-  scale_y_reverse() +
-  xlab("Rank of behavior-brain tissue expression enrichment (by P-value)") +
-  ylab("Rank of T2D onset association strength in PXS (by P-value)") +
-  labs(title = "Relationship between T2D onset strength and brain tissue expression enrichment",
-       subtitle = paste0("r = ", round(cor1$estimate,3), " :: p = ", round(cor1$p.value,3))) +
-  theme_light()
-
-GTEx_general %>% filter(term == "PXS_T2D") %>% arrange(P_adjusted) %>%
-  select(VARIABLE, P_adjusted, significant_P_adj) %>% print(n=15)
-GTEx_specific %>% filter(term == "PXS_T2D") %>% arrange(P_adjusted) %>%
-  select(FULL_NAME, P_adjusted, significant_P_adj) %>% print(n=15)
-
-# repeats the above correlation for all tissue types
-tissue_PXS_GTEx <- GTEx_specific %>%
-  filter(term != "PXS_T2D") %>%
-  select(VARIABLE, P, term, traitname) %>%
-  pivot_wider(
-    names_from = VARIABLE,
-    names_prefix = "P_",
-    values_from = P
-  ) %>%
-  left_join(coeffs %>% select(term, estimate, p.value), by="term")
-PXS_rank <- rank(tissue_PXS_GTEx$p.value)
-#PXS_rank <- (tissue_PXS_GTEx$p.value)
-tissue_PXS_GTEx_results <- tibble(
-  tissue = as.character(), r = as.numeric(), p = as.numeric()
-)
-for (tissue in unique(GTEx_specific$VARIABLE)) {
-  tissue_GTEx_rank <- rank(tissue_PXS_GTEx[[paste0("P_",tissue)]])
-  #tissue_GTEx_rank <- (tissue_PXS_GTEx[[paste0("P_",tissue)]])
-  cor_tissue <- cor.test(PXS_rank, tissue_GTEx_rank)
-  print(paste(tissue, round(cor_tissue$estimate,3), round(cor_tissue$p.value,3)))
-  tissue_PXS_GTEx_results <- tissue_PXS_GTEx_results %>% add_row(
-    tissue = tissue, r = cor_tissue$estimate, p = cor_tissue$p.value
-  )
-}
-tissue_PXS_order <- (tissue_PXS_GTEx_results %>% arrange(p))$tissue
-ggplot(tissue_PXS_GTEx_results, aes(x = factor(tissue, levels = tissue_PXS_order), y = -log10(p))) +
-  geom_col(aes(fill = p < 0.05, color = substring(tissue,1,5)=="Brain"), size=1.25) +
-  geom_abline(slope = 0 , intercept = -log10(0.05)) +
-  scale_color_manual(values=c("white", "black"),
-                     breaks = c(FALSE, TRUE)) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-
-
-# looks at GTEx specific tissue data
+## GTEx Cpecific tissue data ####
 for (i in 1:(nrow(jobID_tbl))) {
   jobID <- jobID_tbl$JobID[i]
   term <- jobID_tbl$term[i]
@@ -133,7 +77,7 @@ for (i in 1:(nrow(jobID_tbl))) {
 GTEx_specific$P_adjusted <- p.adjust(GTEx_specific$P / length(unique(GTEx_specific$VARIABLE)), method = 'bonferroni')
 GTEx_specific$significant_P_adj <- GTEx_specific$P_adjusted < 0.05
 GTEx_specific %>% filter(significant_P_adj) %>% arrange(P_adjusted) %>% print(n=Inf)
-# tile plot
+### tile plot ####
 ggplot(GTEx_specific, aes(x = VARIABLE,
                          y = fct_rev(factor(traitname, levels=expo_order)))) +
   geom_tile(aes(fill=-log10(P_adjusted))) +
@@ -148,7 +92,7 @@ ggplot(GTEx_specific, aes(x = VARIABLE,
   labs(title = "Average gene expression per tissue for each behavior",
        subtitle = "Using MAGMA with GTEx through FUMA. White: p<0.05 (adjusted)")
 
-#### GWAS CATALOG ####
+# GWAS CATALOG ####
 
 # looks at GWAS_catalog data
 for (i in 1:(nrow(jobID_tbl)-1)) {
@@ -207,3 +151,4 @@ GWAS_catalog <- GWAS_catalog %>% left_join(groupings_tbl, by="Trait")
 GWAS_catalog %>%
   group_by(Group) %>% summarize(n = n()) %>%
   filter(!is.na(Group)) %>% arrange(-n)
+GWAS_catalog_trait_count
