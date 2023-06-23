9b6ad42c3cac0ac7b511316bbf803242b4022f3e
nunixnunix04 <nunixnunix@gmail.com>
2023-06-23 14:36:11 -0400

    now runs ldsc h2 analysis on PXS-T2D as well

--- code/aggregate_results.R
+++ code/aggregate_results.R
@@ -1,5 +1,5 @@
 # Need at least 8GB of RAM to load full LMM data later
-## Libraries and directories ##
+# Libraries and directories ####
 library(tidyverse)
 library(data.table)
 # Functions #
@@ -8,6 +8,7 @@ source("paths.R")
 # makes directory for figures
 dir_figs <- paste0(dir_scratch,"figures/")
 dir.create(dir_figs, showWarnings = FALSE)
+dir_results <- paste0(dir_script,"../final_results/")
 
 # paths
 loc_phenolist <- paste0(dir_script,"../input_data/phenotypes.txt")
@@ -15,15 +16,20 @@ pheno_list <- readLines(loc_phenolist)
 loc_expolist <- paste0(dir_script,"../input_data/exposures.txt")
 exposures_list <- readLines(loc_expolist)
 MAGIC_traits <- c("2hGlu","FG","FI","HbA1c")
+# used for plotting purposes
+shortnames <- as_tibble(fread("../input_data/exposures_shortnames.csv"))
+# field information
 fields <- as_tibble(fread(paste0(dir_scratch,"fields_tbl.txt"))) %>%
-  add_row(term = c("T2D","T2D_onset","PXS_T2D", "T2D_all"),
-          traitname = c("Type 2 Diabetes onset","Type 2 Diabetes onset","PXS for Type 2 Diabetes onset","Diabetes (all)")) %>%
-  add_row(term = MAGIC_traits,
-          traitname = c("2-hour Glucose","Fasting Glucose","Fasting Insulin","Glycated haemoglobin (HbA1c)"))
+  left_join(shortnames, by="term") %>%
+  add_row(term = c("T2D","T2D_onset","PXS_T2D", "T2D_all"), use_type="phenotype",
+          shortname = c("T2D onset","T2D onset","PXS for T2D onset","Diabetes (all)")) %>%
+  add_row(term = MAGIC_traits, use_type="MAGIC",
+          shortname = c("2-hour Glucose","Fasting Glucose","Fasting Insulin","Glycated haemoglobin (HbA1c)"))
 #manually shortens long-named traits
-fields[fields$term=="f4080","traitname"] <- "Systolic blood pressure"
+fields$shortname[fields$use_type=="CRF"] <- fields$traitname[fields$use_type=="CRF"]
+fields[fields$term=="f4080","shortname"] <- "Systolic blood pressure"
 
-## Functions ##
+# BOLT-REML results ####
 extract_REML_genCorr <- function(genCorr) {
   N <- length(genCorr)
 
@@ -53,13 +59,10 @@ extract_REML_genCorr <- function(genCorr) {
   out
 }
 
-## Code ##
-
-#### REML results for genCorr PXS with CRFs ####
+## compiles list of gencorr.out results to read ####
 # includes both heritability and genCorr
 gencorr_logs <- tibble(path=as.character(),
                        term1=as.character(), term2=as.character())
-# compiles list of gencorr.out results to read
 for (pheno in pheno_list) {
   loc_CRFs <- paste0(dir_scratch,pheno,"/",pheno,"_CRFs.txt")
   if (!file.exists(loc_CRFs)) {next}
@@ -103,7 +106,7 @@ genCorr_REML_tbl <- tibble(
   gencorr_err = as.numeric(),
   elapsed_hours = as.numeric()
 )
-# actually reads each listed result
+## actually reads each listed result ####
 for (i in 1:nrow(gencorr_logs)) {
   gencorr_log <- readLines(gencorr_logs$path[i])
   out <- extract_REML_genCorr(gencorr_log)
@@ -127,40 +130,40 @@ genCorr_REML_tbl %>% filter(rowSums(is.na(.)) > 0) %>% select(ends_with("term"))
 
 # Adds named terms to table
 genCorr_REML_tbl <- genCorr_REML_tbl %>%
-  left_join(fields %>% select(term, traitname), by=c("pheno1_term"="term")) %>% rename(pheno1_traitname = traitname) %>%
-  left_join(fields %>% select(term, traitname), by=c("pheno2_term"="term")) %>% rename(pheno2_traitname = traitname)
+  left_join(fields %>% select(term, shortname), by=c("pheno1_term"="term")) %>% dplyr::rename(pheno1_shortname = shortname) %>%
+  left_join(fields %>% select(term, shortname), by=c("pheno2_term"="term")) %>% dplyr::rename(pheno2_shortname = shortname)
 
 # Compares CRF gencorr with PXS_T2D vs T2D
 genCorr_REML_tbl %>%
   filter(pheno1_term %in% c("PXS_T2D","T2D")) %>%
-  select(pheno1_term, pheno2_traitname, gencorr, gencorr_err) %>%
+  select(pheno1_term, pheno2_shortname, gencorr, gencorr_err) %>%
   pivot_wider(names_from = pheno1_term,
               values_from = c(gencorr, gencorr_err),
-              id_cols = pheno2_traitname) %>%
+              id_cols = pheno2_shortname) %>%
   arrange(-abs(gencorr_PXS_T2D))
 
 # Compares CRF gencorr with T2D vs T2D_all
 genCorr_REML_tbl %>%
   filter(pheno1_term %in% c("T2D","T2D_all")) %>%
-  select(pheno1_term, pheno2_traitname, gencorr, gencorr_err) %>%
+  select(pheno1_term, pheno2_shortname, gencorr, gencorr_err) %>%
   pivot_wider(names_from = pheno1_term,
               values_from = c(gencorr, gencorr_err),
-              id_cols = pheno2_traitname) %>%
+              id_cols = pheno2_shortname) %>%
   arrange(-abs(gencorr_T2D))
 
 # Saves tables
 loc_out <- paste0(dir_scratch, "genCorr_REML_results.txt")
-write.table(genCorr_REML_tbl, loc_out, sep="\t", quote=FALSE, row.names=FALSE)
+fwrite(genCorr_REML_tbl, loc_out, sep="\t")
 
 # prints out mean absolute gencorr for each CRF
 genCorr_REML_tbl %>% filter(pheno1_term %in% exposures_list) %>%
-  group_by(pheno2_term,pheno2_traitname) %>%
+  group_by(pheno2_term,pheno2_shortname) %>%
   summarize(h2 = mean(h2g2, na.rm=TRUE),
             mean_abs_gencorr= mean(abs(gencorr), na.rm=TRUE)) %>%
   arrange(-mean_abs_gencorr)
 # prints out mean absolute gencorr for each exposure
 genCorr_REML_tbl %>% filter(pheno1_term %in% exposures_list) %>%
-  group_by(pheno1_term,pheno1_traitname) %>%
+  group_by(pheno1_term,pheno1_shortname) %>%
   summarize(h2 = mean(h2g1, na.rm=TRUE),
             mean_abs_gencorr= mean(abs(gencorr), na.rm=TRUE)) %>%
   arrange(-mean_abs_gencorr) %>% print(n=Inf)
@@ -168,30 +171,32 @@ genCorr_REML_tbl %>% filter(pheno1_term %in% exposures_list) %>%
 genCorr_REML_tbl%>%filter(pheno1_term=="PXS_T2D") %>% arrange(-abs(gencorr))
 (genCorr_REML_tbl%>%filter(pheno1_term=="PXS_T2D"))$gencorr %>% abs() %>% mean()
 
+## Gencorr Visualization ####
+
 # gets order of exposures from decreasing p-value
 coeffs <- as_tibble(fread(paste0(dir_script,"../input_data/PXS_coefficients.txt")))
-expo_order <- factor(c("PXS for Type 2 Diabetes onset",
+expo_order <- factor(c(shortnames$shortname[1],
                        (coeffs %>% filter(term %in% exposures_list) %>% 
-                          left_join(fields%>%select(term,traitname), by="term") %>%
-                          arrange(p.value))$traitname) )
-CRF_order <- factor((genCorr_REML_tbl%>%filter(pheno1_term=="PXS_T2D") %>% arrange(-abs(gencorr)))$pheno2_traitname)
+                          left_join(fields%>%select(term,shortname), by="term") %>%
+                          arrange(p.value))$shortname) )
+CRF_order <- factor((genCorr_REML_tbl%>%filter(pheno1_term=="PXS_T2D") %>% arrange(-abs(gencorr)))$pheno2_shortname)
 # calculates Bonferroni-corrected confidence intervals
 b1 <- qnorm(1 - (0.025 / nrow(genCorr_REML_tbl %>% filter(pheno1_term %in% exposures_list))))
 # Visualizes exposure x CRF gencorrs
-ggplot(genCorr_REML_tbl %>% filter(pheno1_traitname %in% expo_order),
-       aes(x=factor(pheno2_traitname,levels=CRF_order),
-           y=fct_rev(factor(pheno1_traitname,levels=expo_order)),
+ggplot(genCorr_REML_tbl %>% filter(pheno1_shortname %in% expo_order),
+       aes(x=factor(pheno2_shortname,levels=CRF_order),
+           y=fct_rev(factor(pheno1_shortname,levels=expo_order)),
            fill=gencorr)) +
   geom_tile() +
   geom_text(data=genCorr_REML_tbl %>%
-              filter(pheno1_traitname %in% expo_order, abs(gencorr)-b1*gencorr_err > 0),
+              filter(pheno1_shortname %in% expo_order, abs(gencorr)-b1*gencorr_err > 0),
             aes(label = formatC(signif(gencorr,digits=2), digits=2,format="fg", flag="#"))) +
   geom_rect(aes(xmin = -Inf, xmax = Inf,
                 ymin = length(expo_order)-0.5, ymax = length(expo_order)+0.5),
             color = "black", fill = NA) +
   scale_fill_gradient2(low="red",mid="white", high="green", midpoint = 0) +
   scale_x_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width=20), expand = c(0, 0)) +
-  scale_y_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width=40), expand = c(0, 0)) +
+  scale_y_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width=20), expand = c(0, 0)) +
   xlab("Clinical Risk Factor (CRF)") +
   ylab("Behavior") +
   labs(title = "Genetic Correlations between behaviors + PXS-T2D and CRFs",
@@ -204,14 +209,14 @@ loc_fig <- paste0(dir_figs,"genCorrs_PXS-expos_CRFs.png")
 ggsave(loc_fig,width=4000, height=3000, units="px")
 
 # looks at heritabilities of traits
-genCorr_REML_tbl %>% group_by(pheno1_term,pheno1_traitname) %>%
+genCorr_REML_tbl %>% group_by(pheno1_term,pheno1_shortname) %>%
   filter(h2g1_err == min(h2g1_err)) %>%
   summarize(h2 = mean(h2g1), h2_err = mean(h2g1_err)) %>%
   mutate(h2_low = h2 - 1.96*h2_err, h2_upp = h2 + 1.96*h2_err) %>%
   arrange(-h2) %>% print(n=Inf)
 
-#### LDsc results ####
-##### genCorr results from LDsc #####
+# LDsc results ####
+## genCorr results from LDsc ####
 extract_LDsc_genCorr <- function(gencorr_log) {
   N <- length(gencorr_log)
   
@@ -252,9 +257,10 @@ extract_LDsc_genCorr <- function(gencorr_log) {
            gencorr, gencorr_se, gencorr_Z, gencorr_P)
   out
 }
-
+### makes list of .log files to read ####
 terms1 <- c("PXS_T2D","PXS_T2D","T2D_onset")
 terms2 <- c("T2D_onset","T2D_all","T2D_all")
+
 ldsc_logs <- tibble(
   path=paste0(dir_scratch,"MAGIC/MAGIC_",MAGIC_traits,"_PXS_rg.log"),
   term1="PXS_T2D", term2=MAGIC_traits
@@ -283,7 +289,7 @@ genCorr_LDsc_tbl <- tibble(
   gencorr_Z = as.numeric(),
   gencorr_P = as.numeric()
 )
-# loops through each MAGIC trait
+### loops through each MAGIC trait ####
 for (i in 1:nrow(ldsc_logs)) {
   # extracts ldsc gencorr data
   loc_gencorr_log <- ldsc_logs$path[i]
@@ -321,17 +327,17 @@ genCorr_LDsc_tbl$significant <- genCorr_LDsc_tbl$gencorr_P_adj < 0.05
 
 # Adds named terms to table
 genCorr_LDsc_tbl <- genCorr_LDsc_tbl %>%
-  left_join(fields %>% select(term, traitname), by=c("pheno1_term"="term")) %>% rename(pheno1_traitname = traitname) %>%
-  left_join(fields %>% select(term, traitname), by=c("pheno2_term"="term")) %>% rename(pheno2_traitname = traitname)
+  left_join(fields %>% select(term, shortname), by=c("pheno1_term"="term")) %>% dplyr::rename(pheno1_shortname = shortname) %>%
+  left_join(fields %>% select(term, shortname), by=c("pheno2_term"="term")) %>% dplyr::rename(pheno2_shortname = shortname)
 
 # Views significant gencorrs
 genCorr_LDsc_tbl %>% filter(significant) %>%
   select(-contains("h2"), -gencorr_Z, -gencorr_P, - ends_with("lambda"), -ends_with("term"))
 
-# Visualizes exposure x CRF gencorrs
+### Visualizes exposure x CRF gencorrs ####
 ggplot(genCorr_LDsc_tbl[c(i_PXS_MAGIC,i_expos_MAGIC),],
-       aes(x=pheno2_traitname,#factor(pheno2_traitname,levels=CRF_order),
-           y=fct_rev(factor(pheno1_traitname,levels=expo_order)),
+       aes(x=pheno2_shortname,#factor(pheno2_shortname,levels=CRF_order),
+           y=fct_rev(factor(pheno1_shortname,levels=expo_order)),
            fill=gencorr)) +
   geom_tile() +
   geom_text(data=genCorr_LDsc_tbl[c(i_PXS_MAGIC,i_expos_MAGIC),] %>% filter(significant),
@@ -341,7 +347,7 @@ ggplot(genCorr_LDsc_tbl[c(i_PXS_MAGIC,i_expos_MAGIC),],
             color = "black", fill = NA) +
   scale_fill_gradient2(low="red",mid="white", high="green", midpoint = 0) +
   scale_x_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width=20), expand = c(0, 0)) +
-  scale_y_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width=40), expand = c(0, 0)) +
+  scale_y_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width=20), expand = c(0, 0)) +
   xlab("MAGIC Phenotype") +
   ylab("Behavior") +
   labs(title = "Genetic Correlations between behaviors + PXS-T2D and MAGIC phenotypes",
@@ -362,7 +368,7 @@ ggsave(loc_fig,width=4000, height=3000, units="px")
 
 
 
-##### h2 results from LDsc #####
+## h2 results from LDsc ####
 # function for extracting data from ldsc h2 log file
 extract_LDsc_h2 <- function(h2_log) {
   N <- length(h2_log)
@@ -391,10 +397,14 @@ extract_LDsc_h2 <- function(h2_log) {
   ratio <- as.numeric(str_split(line_split, " ")[[1]][1])
   ratio_se <- parse_number(str_split(line_split, " ")[[1]][2])
   
-  
   out <- c(h2, h2_se, lambda, mean_chi2, intercept, intercept_se, ratio, ratio_se)
   out
 }
+### makes list of ldsc log files to go through
+ldsc_h2_logs <- tibble(
+    path = paste0(dir_scratch,"exposures/",exposures_list,"/ldsc_h2_",exposures_list,".log"),
+    term = exposures_list
+  ) %>% add_row( path = paste0(dir_scratch,"T2D/ldsc_h2_PXS_T2D.log"), term = "PXS_T2D" )
 
 h2_LDsc_tbl <- tibble(
   term = as.character(),
@@ -407,30 +417,33 @@ h2_LDsc_tbl <- tibble(
   ratio = as.numeric(),
   ratio_se = as.numeric()
 )
-# loops through each exposure
-for (expo in exposures_list) {
-  loc_h2_log <- paste0(dir_scratch,"exposures/",expo,"/ldsc_h2_",expo,".log")
+### loops through each exposure ####
+for (i in 1:nrow(ldsc_h2_logs)) {
+  loc_h2_log <- ldsc_h2_logs$path[i]
+  term <- ldsc_h2_logs$term[i]
+  #loc_h2_log <- paste0(dir_scratch,"exposures/",expo,"/ldsc_h2_",expo,".log")
   # skips if log file is missing
-  if (!file.exists(loc_h2_log)) {print(paste("Missing log file for",expo)); next}
-  print(paste("Reading log file for", expo))
+  if (!file.exists(loc_h2_log)) {print(paste("Missing log file for",term)); next}
+  print(paste("Reading log file for", term))
   h2_log <- readLines(loc_h2_log)
   if (length(h2_log)==0) {print("Log file is empty!"); next}
   out <- extract_LDsc_h2(h2_log)
   # adds data to table
   h2_LDsc_tbl <- h2_LDsc_tbl %>% add_row(
-    term=expo, h2=out[1], h2_se=out[2], lambda=out[3], mean_chi2=out[4],
+    term=term, h2=out[1], h2_se=out[2], lambda=out[3], mean_chi2=out[4],
     intercept=out[5], intercept_se=out[6], ratio=out[7], ratio_se=out[8]
   )
 }
 # appends trait names
-h2_LDsc_tbl <- h2_LDsc_tbl %>% left_join(fields[,c("term","traitname")], by="term")
+h2_LDsc_tbl <- h2_LDsc_tbl %>% left_join(fields[,c("term","shortname")], by="term")
 
 # compares BOLT-REML vs LDsc in h2 computation
 h2_comparison <- h2_LDsc_tbl %>%
-  select(term, traitname, ldsc_h2=h2,ldsc_h2_se=h2_se,ldsc_lambda=lambda, ldsc_ratio=ratio) %>%
+  select(term, shortname, ldsc_h2=h2,ldsc_h2_se=h2_se,ldsc_lambda=lambda, ldsc_ratio=ratio) %>%
   left_join(genCorr_REML_tbl %>% group_by(pheno1_term) %>%
               summarize(REML_h2 = mean(h2g1), REML_h2_err = mean(h2g1_err)),
             by = c("term"="pheno1_term"))
+### ldsc vs REML comparison ####
 # shows h2 comparisons after LDsc's GC adjustment
 (lm1 <- lm(ldsc_h2 ~ REML_h2, data = h2_comparison))
 ggplot(h2_comparison, aes(x=REML_h2, y=ldsc_h2)) +
@@ -439,8 +452,12 @@ ggplot(h2_comparison, aes(x=REML_h2, y=ldsc_h2)) +
   geom_errorbar(aes(ymin = ldsc_h2-2*ldsc_h2_se, ymax = ldsc_h2+2*ldsc_h2_se)) +
   geom_errorbarh(aes(xmin = REML_h2-2*REML_h2_err, xmax = REML_h2+2*REML_h2_err)) +
   geom_point() +
-  xlim(0,0.15) + ylim(0,0.15) +
+  xlim(0,0.2) + ylim(0,0.2) +
   xlab("h2 estimated by BOLT-REML") +
   ylab("h2 estimated by LDsc") +
   labs(title = "Comparison of h2 estimate of 25 exposures by LDsc vs BOLT-REML",
        subtitle = paste0("LDsc_h2 ~ BOLT-REML_h2 slope = ", round(lm1$coefficients[2],3)))
+
+# prints table
+loc_out <- paste0(dir_results, "h2_ldsc_REML.txt")
+fwrite(h2_comparison, loc_out, sep="\t")
