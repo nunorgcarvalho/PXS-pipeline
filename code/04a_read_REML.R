d88f36459d06273d61b5c3323dbd77019ce94869
nunixnunix04 <nunixnunix@gmail.com>
2025-03-02 22:58:59 -0500

    bar charts for BRS h2g and gencorr results

--- code/04a_read_REML.R
+++ code/04a_read_REML.R
@@ -7,7 +7,7 @@ source('code/00_paths.R')
 
 # REML gencorrs ####
 dir_gencorrs <- paste0(dir_scratch, 'gencorrs/')
-rg_files_out <- list.files(dir_gencorrs, pattern='*.out')
+rg_files_out <- list.files(dir_gencorrs, pattern='^REML.*\\.out$')
 
 gencorr_REML <- tibble(file='', h2_cohort='',
                        trait1='',trait2='',
@@ -17,6 +17,10 @@ gencorr_REML <- tibble(file='', h2_cohort='',
 for (rg_file in rg_files_out) {
   print(rg_file)
   file_split <- str_split(rg_file,'\\.')[[1]]
+  if (length(file_split) == 5) {
+    h2_cohort <- file_split[2]
+    file_split <- file_split[-2]
+  } else {h2_cohort <- NA}
   rg_out <- readLines(paste0(dir_gencorrs, rg_file))
   
   ## reads rg out file ####
@@ -46,15 +50,88 @@ for (rg_file in rg_files_out) {
   
   
   gencorr_REML <- gencorr_REML %>% add_row(
-    file=rg_file, h2_cohort=file_split[2],
-    trait1=file_split[3], trait2=file_split[4],
+    file=rg_file, h2_cohort=h2_cohort,
+    trait1=file_split[2], trait2=file_split[3],
     trait1_h2g=h2g1, trait1_h2g_err=h2g1_err,
     trait2_h2g=h2g2, trait2_h2g_err=h2g2_err,
     rg=gencorr, rg_err=gencorr_err, elapsed_hours=elapsed_hours
   )
 }
 
+# BRS vs CRFs comparisons
+col_BRS <- 'BRS-ALL-cov_bvr' # main BRS term
 
+
+fields <- as_tibble(fread('scratch/fields_tbl.txt')) %>%
+  add_row(term='CRS-ALL', use_type='CRF',
+          traitname = 'Clinical Risk Score (CRS)')
+col_CRFs <- fields$term[fields$use_type == 'CRF']
+
+CI95_z <- qnorm(0.975)
+gc1 <- gencorr_REML %>%
+  filter(trait1 == col_BRS,
+         trait2 %in% col_CRFs) %>%
+  left_join(fields %>% filter(use_type=='CRF') %>%
+              select(trait2=term, traitname), by='trait2') %>%
+  arrange(-abs(rg)) %>%
+  mutate(rg_low = rg - CI95_z*rg_err,
+         rg_upp = rg + CI95_z*rg_err,
+         sign = ifelse(rg>0,'Positive','Negative'),
+         traitname = factor(traitname))
+
+ggplot(gc1, aes(x=factor(traitname, levels=traitname), y=abs(rg))) +
+  geom_col(aes(fill = sign)) +
+  geom_errorbar(aes(ymin=abs(rg_low), ymax=abs(rg_upp)),
+                width=0.5) +
+  coord_flip() +
+  scale_x_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width=20)) +
+  scale_y_continuous(expand=c(0,0,0.1,0)) +
+  labs(x = 'Clinical Risk Factors and Score',
+       y = 'Absolute genetic correlation (95% CI) with Behavioral Risk Score (BRS)',
+       fill = 'Sign') +
+  theme_bw() +
+  theme(legend.position = 'top')
+  
+
+# Heritability of behaviors and score ####
+col_bvrs <- fields$term[fields$use_type == 'behavior']
+col_bvrs_ <- str_replace_all(col_bvrs,'\\.','_')
+hg1 <- gencorr_REML %>%
+  filter(trait2 %in% c(col_bvrs_,col_BRS)) %>%
+  group_by(trait2) %>%
+  summarize(h2 = mean(trait2_h2g),
+            h2_err = mean(trait2_h2g_err)) %>%
+  mutate(term = ifelse(trait2 == col_BRS, trait2,
+                       str_replace_all(trait2,'_','\\.'))) %>%
+  arrange(-h2) %>%
+  left_join(fields %>% filter(use_type=='behavior') %>%
+              mutate(traitname = str_remove(traitname, "\\s*\\([^)]*\\)")) %>%
+              add_row(term = col_BRS, traitname = 'Behavioral Risk Score (BRS)') %>%
+              select(term, traitname), by='term')
+# temporary, for missing data
+hg1$h2[is.na(hg1$h2)] <- 0.005
+hg1$h2_err[is.na(hg1$h2_err)] <- 0.0025
+hg1 <- hg1 %>% mutate(h2_low = h2 - CI95_z*h2_err,
+                      h2_upp = h2 + CI95_z*h2_err)
+
+
+ggplot(hg1, aes(x=factor(traitname, levels=traitname), y=h2)) +
+  geom_col(aes(fill = term==col_BRS)) +
+  geom_errorbar(aes(ymin=abs(h2_low), ymax=abs(h2_upp)),
+                width=0.5) +
+  coord_flip() +
+  scale_x_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width=40)) +
+  scale_y_continuous(expand=c(0,0,0.1,0),
+                     breaks = seq(0,1,5/100),
+                     minor_breaks = seq(0,1,1/100)) +
+  labs(x = 'Behaviors and BRS',
+       y = 'Heritability (h2)') +
+  theme_bw() +
+  theme(legend.position = 'none',
+        axis.text.y = element_text(size=5))
+
+
+#############################################################
 # clean table ####
 Cstats <- as_tibble(fread('scratch/BRS_models/BRS_Cstats.txt'))
 
