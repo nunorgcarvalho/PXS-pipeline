00b542769fd40d4ab3ae2d5f43595739d67d9e18
nunixnunix04 <nunixnunix@gmail.com>
2025-04-05 16:08:04 -0400

    finalized pheno-corr and gen-corr plots

--- code/00_plotting.R
+++ code/00_plotting.R
@@ -11,40 +11,23 @@ dash_color <- 'gray50'
 
 theme_set(theme_bw()) # default theme is yuck
 
+# shared theme for multiple tile plots
+tileplot_theme <- theme(
+  legend.key.size = unit(2, "mm"),
+  legend.title = element_text(size=6),
+  legend.text = element_text(size=5),
+  legend.margin = margin(0,-1,0,-2, unit="mm"),
+  legend.text.align = 1,
+  plot.title = element_text(size=7),
+  plot.subtitle = element_text(size=5),
+  axis.title = element_text(size=6),
+  axis.text = element_text(size=3.5 , lineheight=0.8)
+)
+
 # functions ####
 
 # formats a correlation object as: r = 0.120 (p = 3.0x10^-7)
-# using parse=FALSE (such as inside annotate)
-annotate_cor.test <- function(
-    cor_obj,
-    r_digits = 3,
-    p_digits = 2,
-    p_limit1 = 0.001,
-    p_limit2 = 2.2e-16
-) {
-  r_val <- formatC(cor_obj$estimate, digits = r_digits, format = "f")
-  p <- cor_obj$p.value
-  comp <- "=="
-  
-  if (p > p_limit1) {
-    p_val <- formatC(p, digits = point_digits, format = "f")
-    p_expr <- bquote(p == .(p_val))
-  } else {
-    if (p == 0) {
-      p <- p_limit2
-      comp <- "<"
-    }
-    p_exp <- floor(log10(p))
-    p_base <- p / 10^p_exp
-    p_val <- formatC(p_base, digits = p_digits - 1, format = "f")
-    p_expr <- bquote(p *.(comp)* .(p_val) %*% 10^.(p_exp))
-  }
-  
-  return( bquote(r == .(r_val) ~ "(" * .(p_expr) * ")") )
-}
-
-
-
+# using bquote() (set parse=FALSE, such as inside annotate)
 annotate_cor.test <- function(
     cor_obj,
     r_digits = 3,
@@ -61,18 +44,20 @@ annotate_cor.test <- function(
   return( bquote(.(r_expr) ~ "(" * .(p_expr) * ")") )
 }
 
-
-
+# formats a correlation value as: r = 0.120
+# using bquote() (set parse=FALSE, such as inside annotate)
 get_r_expr <- function(r, r_digits = 3) {
   r_val <- formatC(r, digits = r_digits, format = "f")
   r_expr <- bquote(r == .(r_val))
   return(r_expr)
 }
 
+# formats a p-value as: p = 3.0x10^-7
+# using bquote() (set parse=FALSE, such as inside annotate)
 get_p_expr <- function(p, p_digits = 2, p_limit1 = 0.001, p_limit2 = 2.2e-16) {
-  comp <- "=="
+  comp <- '=='
   if (p > p_limit1) {
-    p_val <- formatC(p, digits = point_digits, format = "f")
+    p_val <- formatC(p, digits = p_digits, format = "f")
     p_expr <- bquote(p == .(p_val))
   } else {
     if (p == 0) {
@@ -82,7 +67,9 @@ get_p_expr <- function(p, p_digits = 2, p_limit1 = 0.001, p_limit2 = 2.2e-16) {
     p_exp <- floor(log10(p))
     p_base <- p / 10^p_exp
     p_val <- formatC(p_base, digits = p_digits - 1, format = "f")
-    p_expr <- bquote(p *.(comp)* .(p_val) %*% 10^.(p_exp))
+    if (comp == '==') {p_expr <- bquote(p == .(p_val) %*% 10^.(p_exp))
+    } else {p_expr <- bquote(p < .(p_val) %*% 10^.(p_exp))}
+    
   }
   
   return(p_expr)
--- code/07b_rg_plots.R
+++ code/07b_rg_plots.R
@@ -4,48 +4,37 @@
 library(tidyverse)
 library(data.table)
 source('code/00_paths.R')
+source('code/00_plotting.R')
 
 shortnames <- as_tibble(fread(paste0(dir_repo,'input_data/term_shortnames.tsv'))) %>%
-  select(term, shortname, category) %>%
-  filter(!(term %in% c('BRS-ALL-cov_bvr','CRS-ALL','PRS_T2D')))
-
+  select(term, shortname, category)
 cat_rle <- rle(shortnames$category)
 cat_tbl <- tibble(category=cat_rle$values,
                   i_stop = cumsum(cat_rle$lengths),
                   i_start = c(1,head(i_stop + 1,-1)) )
-
-# shared theme for multiple plots
-tileplot_theme <- theme(
-  legend.key.size = unit(2, "mm"),
-  legend.title = element_text(size=6),
-  legend.text = element_text(size=5),
-  legend.margin = margin(0,-1,0,-2, unit="mm"),
-  legend.text.align = 1,
-  plot.title = element_text(size=7),
-  plot.subtitle = element_text(size=5),
-  axis.title = element_text(size=6),
-  axis.text = element_text(size=5)
-)
-
 # adds dashed lines to denote categories
-add_cat_lines <- function(gg, i_start) {
+add_cat_lines <- function(gg, i_start, dash_size=0.5) {
   for (i in 1:length(i_start)) {
     gg <- gg +
       annotate('segment', x=i_start[i]-0.5, xend=i_start[i]-0.5,
-               y=-Inf,yend=Inf, linetype='dashed', color = "gray40", size=0.5) +
+               y=-Inf,yend=Inf, linetype='dashed', color = "gray40", dash_size) +
       annotate('segment', y=i_start[i]-0.5, yend=i_start[i]-0.5,
-               x=-Inf,xend=Inf, linetype='dashed', color = "gray40", size=0.5)
+               x=-Inf,xend=Inf, linetype='dashed', color = "gray40", dash_size)
   }
   return(gg)
 }
 
 
 # phenotypic correlation ####
+col_BRS <- 'BRS-ALL-cov_bvr'
+shortnames_corr <- shortnames %>% filter(term == col_BRS | category !='') %>%
+  arrange(term != col_BRS) %>% select(!category)
+boldings <- shortnames_corr$term == col_BRS
 corr_tbl <- as_tibble(fread(paste0(dir_scratch,'general_results/behavior_corr_table.tsv'))) %>%
-  left_join(shortnames %>% rename(shortname1=shortname), by=c('term1'='term')) %>%
-  left_join(shortnames %>% rename(shortname2=shortname), by=c('term2'='term')) %>%
-  mutate(shortname1 = factor(shortname1, levels=shortnames$shortname),
-         shortname2 = factor(shortname2, levels=shortnames$shortname))
+  left_join(shortnames %>% select(term1=term,shortname1=shortname), by='term1') %>%
+  left_join(shortnames %>% select(term2=term,shortname2=shortname), by='term2') %>%
+  mutate(shortname1 = factor(shortname1, levels=shortnames_corr$shortname),
+         shortname2 = factor(shortname2, levels=shortnames_corr$shortname))
 
 gg1 <- ggplot(corr_tbl, aes(x=shortname1, y=shortname2, fill=r)) +
   geom_tile() +
@@ -59,26 +48,26 @@ gg1 <- ggplot(corr_tbl, aes(x=shortname1, y=shortname2, fill=r)) +
   tileplot_theme +
   theme(
     axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-    panel.grid = element_blank()
+    panel.grid = element_blank(),
+    axis.text = element_text(face = ifelse(boldings, 'bold','plain'))
   )
-gg1 <- add_cat_lines(gg1, cat_tbl$i_start)
+gg1 <- add_cat_lines(gg1, cat_tbl$i_start+1)
 gg1
 
 
 
 # genotypic correlations ####
-shortnames_rg <- shortnames[c(49,1:48),] # manual code, I'm sorry
-terms_keep <- shortnames_rg$term
 rg_tbl <- as_tibble(fread(paste0(dir_scratch,'general_results/behavior_rg_table.tsv'))) %>%
   select(-starts_with('h2g'),-starts_with('intercept'), -file) %>%
   mutate(across(starts_with('term'), function(x) str_replace_all(x,'_','\\.')))
-rg_tbl <- rg_tbl %>% add_row(rg_tbl %>% rename(term1=term2,term2=term1))
+rg_tbl <- rg_tbl %>% add_row(rg_tbl %>% rename(term1=term2,term2=term1)) %>%
+  mutate(across(c(term1,term2), function(x) str_replace(x, 'BRS', col_BRS)))
 
-rg_tbl1 <- rg_tbl %>% filter(term1 %in% terms_keep, term2 %in% terms_keep) %>%
-  left_join(shortnames_rg %>% select(term1=term, shortname1=shortname), by='term1') %>%
-  left_join(shortnames_rg %>% select(term2=term, shortname2=shortname), by='term2') %>%
-  mutate(shortname1 = factor(shortname1, levels=shortnames_rg$shortname),
-         shortname2 = factor(shortname2, levels=shortnames_rg$shortname))
+rg_tbl1 <- rg_tbl %>% filter(term1 %in% shortnames_corr$term, term2 %in% shortnames_corr$term) %>%
+  left_join(shortnames_corr %>% select(term1=term, shortname1=shortname), by='term1') %>%
+  left_join(shortnames_corr %>% select(term2=term, shortname2=shortname), by='term2') %>%
+  mutate(shortname1 = factor(shortname1, levels=shortnames_corr$shortname),
+         shortname2 = factor(shortname2, levels=shortnames_corr$shortname))
 
 
 gg2 <- ggplot(rg_tbl1, aes(x=shortname1, y=shortname2, fill=rg)) +
@@ -93,7 +82,77 @@ gg2 <- ggplot(rg_tbl1, aes(x=shortname1, y=shortname2, fill=rg)) +
   tileplot_theme +
   theme(
     axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-    panel.grid = element_blank()
+    panel.grid = element_blank(),
+    axis.text = element_text(face = ifelse(boldings, 'bold','plain'))
   )
 gg2 <- add_cat_lines(gg2, cat_tbl$i_start+1)
 gg2
+
+
+# joint corr + rg plot ####
+joint_tbl <- tibble(id1=0,id2=0,shortname1='',shortname2='', type='', corr=0)[0,]
+for (i in 1:length(shortnames_corr$shortname)) {
+  name1 <- shortnames_corr$shortname[i]
+  for (j in i:length(shortnames_corr$shortname)) {
+    if (j == i) {next}
+    name2 <- shortnames_corr$shortname[j]
+    pheno_corr <- corr_tbl$r[corr_tbl$shortname1==name1 & corr_tbl$shortname2==name2]
+    gen_corr <- rg_tbl1$rg[rg_tbl1$shortname1==name1 & rg_tbl1$shortname2==name2]
+    joint_tbl <- joint_tbl %>% add_row(id1=i, id2=j,
+      shortname1=c(name1,name2), shortname2=c(name2,name1),
+      type=c('pheno','geno'), corr = c(pheno_corr, gen_corr)
+    )
+
+  }
+}
+joint_tbl <- joint_tbl %>% mutate(
+  across(starts_with('shortname'),
+         function(x) factor(x, levels = shortnames_corr$shortname)))
+
+gg3 <- ggplot(joint_tbl, aes(x=shortname1, y=shortname2, fill=corr)) +
+  geom_tile() +
+  geom_text(aes(label = sub("(^|-)0", "\\1", sprintf("%.2f", corr))), size = 1.25) +
+  scale_fill_gradient2(low="#DE1B1B",mid="white", high="#93CF1A", na.value='white',
+                       midpoint = 0, limits = c(-1, 1)) +
+  scale_x_discrete(labels = function(x) str_wrap(x, width=20), expand = c(0, -10)) +
+  scale_y_discrete(labels = function(x) str_wrap(x, width=20), expand = c(0, 0)) +
+  labs(x='Behavior', y='Behavior', fill='r') +
+  coord_fixed(expand = FALSE) +
+  tileplot_theme +
+  theme(
+    axis.text = element_text(face = ifelse(boldings, 'bold','plain')),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+    panel.grid = element_blank()
+  )
+gg3 <- add_cat_lines(gg3, cat_tbl$i_start+1, dash_size=0.25)
+gg3
+loc_fig <- paste0(dir_figs,"corr_megatable")
+ggsave(paste0(loc_fig,".png"), width=180, height=180, units="mm", dpi=900)
+
+## comparing pheno vs genetic correlations ####
+joint_tbl2 <- joint_tbl %>% filter(type=='pheno') %>%
+  select(id1,id2, shortname1, shortname2, pheno_corr=corr) %>%
+  left_join(joint_tbl %>% filter(type=='geno') %>%
+              select(id1,id2, geno_corr=corr),
+            by=c('id1','id2')) %>%
+  mutate(corr_diff = geno_corr - pheno_corr) %>%
+  arrange(-abs(corr_diff))
+
+cor_corrs <- cor.test(joint_tbl2$pheno_corr, joint_tbl2$geno_corr)
+ggplot(joint_tbl2, aes(x=pheno_corr, geno_corr)) +
+  geom_point(shape=1) +
+  geom_abline(slope=1,linetype='dashed', color=dash_color) +
+  geom_hline(yintercept=0,linetype='dashed', color=dash_color) +
+  geom_vline(xintercept=0,linetype='dashed', color=dash_color) +
+  #geom_smooth(method='lm',formula='y~x') +
+  annotate('text', label = annotate_cor.test(cor_corrs), # throws out harmless warning message
+           x=Inf, y=-Inf, vjust=-0.5, hjust=1.05, parse=FALSE) +
+  scale_x_continuous(limits=c(-1,1), expand=c(0,0)) +
+  scale_y_continuous(limits=c(-1,1), expand=c(0,0)) +
+  labs(x='Phenotypic correlation',
+       y='Genetic correlation') +
+  coord_fixed() +
+  theme(plot.margin = margin(r = 2.5, unit = "mm"))
+
+loc_fig <- paste0(dir_figs,"corr_comparison")
+ggsave(paste0(loc_fig,".png"), width=120, height=120, units="mm", dpi=300)
