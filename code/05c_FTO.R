2c573e2b375bdbd12aba8a78f2a8f1ebc52ea69d
nunixnunix04 <nunixnunix@gmail.com>
2025-05-02 17:04:23 -0400

    updated FTO gene analysis

--- code/05c_FTO.R
+++ code/05c_FTO.R
@@ -4,70 +4,54 @@ source('code/paths.R')
 
 dir_FUMA <- paste0(dir_scratch,"FUMA_results/")
 # Shared code ####
-# used for plotting purposes
-shortnames <- as_tibble(fread(paste0(dir_script,"../input_data/exposures_shortnames.csv")))
-# loads job ID table to match jobs to fields
-fields <- as_tibble(fread(paste0(dir_scratch,"fields_tbl.txt"))) %>%
-  add_row(term="PXS_T2D", traitname="PXS for Type 2 Diabetes onset") %>%
-  left_join(shortnames, by="term")
-jobID_tbl <- as_tibble(fread(paste0(dir_FUMA,"jobid_name.csv"))) %>%
-  mutate(term = substring(JobName,5))
-jobID_tbl[jobID_tbl$JobName=="PXS NC", "term"] <- "PXS_T2D"
-jobID_tbl <- jobID_tbl %>%
-  left_join(fields %>% select(term, traitname, shortname), by="term") %>%
-  select(-V4)
-
 
 # FTO SNPs ####
-
+list_FUMA_SNPs <- list.files(dir_FUMA, pattern='snps.txt', recursive=TRUE)
 ## extracts FUMA SNP results ####
-for (i in 1:(nrow(jobID_tbl))) {
-  jobID <- jobID_tbl$JobID[i]
-  term <- jobID_tbl$term[i]
-  shortname <- jobID_tbl$shortname[i]
-  print(paste(i,jobID,term,shortname))
+for (i in 1:length(list_FUMA_SNPs)) {
+  file <- list_FUMA_SNPs[i]
+  loc_file <- paste0(dir_FUMA, file)
+  print(paste(i,file))
   
-  loc_file <- paste0(dir_FUMA,"FUMA_job",jobID,"/snps.txt")
   job_data <- as_tibble(fread(loc_file, skip="uniqID"))
+  if (nrow(job_data)==0) {next}
   
   if (i==1) {
-    SNPs <- job_data %>% select(uniqID, rsID, chr, pos, gwasP, IndSigSNP, nearestGene) %>%
-      mutate(term = term, shortname = shortname)
+    SNPs <- job_data %>% select(uniqID, rsID, chr, pos, gwasP, IndSigSNP, nearestGene)
   } else {
     SNPs <- SNPs %>%
-      add_row(job_data %>% select(uniqID, rsID, chr, pos, gwasP, IndSigSNP, nearestGene) %>%
-      mutate(term = term, shortname = shortname) )
+      add_row(job_data %>% select(uniqID, rsID, chr, pos, gwasP, IndSigSNP, nearestGene))
   }
 }
-FTO_terms <- (SNPs %>% filter(nearestGene == "FTO") )$term %>% unique()
 FTO_SNPs <- (SNPs %>% filter(nearestGene == "FTO") )
 FTO_rsIDs <- FTO_SNPs$rsID %>% unique()
 
-window <- 50000
-LMM_PXS_T2D <- as_tibble(fread("scratch/T2D/LMM_PXS_T2D_bgen.txt"))
-LMM_PXS_T2D_FTO <- LMM_PXS_T2D %>%
+window <- 250000
+LMM_BRS <- as_tibble(fread(paste0(dir_scratch, "LMM/LMM.ALL.BRS-ALL-cov_bvr.bgen.txt")))
+LMM_BRS_FTO <- LMM_BRS %>%
   filter(BP >= min(FTO_SNPs$pos) - window , BP <= max(FTO_SNPs$pos) + window,
          CHR == median(FTO_SNPs$chr)) %>%
   mutate(FTO_SNP = (BP >= min(FTO_SNPs$pos) & BP <= max(FTO_SNPs$pos)) )
-  #mutate(FTO_SNP = SNP %in% FTO_rsIDs )
-LMM_PXS_T2D_FTO %>% arrange(P_BOLT_LMM_INF) %>% select(P_BOLT_LMM_INF, everything()) %>% View()
-mean_log10P <- LMM_PXS_T2D_FTO %>% group_by(FTO_SNP) %>%
+
+LMM_BRS_FTO %>% arrange(P_BOLT_LMM_INF) %>% select(P_BOLT_LMM_INF, everything()) %>%
+  filter(FTO_SNP) %>% View()
+mean_log10P <- LMM_BRS_FTO %>% group_by(FTO_SNP) %>%
   summarize(mean_log10P = mean(-log10(P_BOLT_LMM_INF)))
-tt1 <- t.test(-log10(LMM_PXS_T2D_FTO[LMM_PXS_T2D_FTO$FTO_SNP,"P_BOLT_LMM_INF"]) ,
-       -log10(LMM_PXS_T2D_FTO[!LMM_PXS_T2D_FTO$FTO_SNP,"P_BOLT_LMM_INF"]) )
+tt1 <- t.test(-log10(LMM_BRS_FTO[LMM_BRS_FTO$FTO_SNP,"P_BOLT_LMM_INF"]) ,
+       -log10(LMM_BRS_FTO[!LMM_BRS_FTO$FTO_SNP,"P_BOLT_LMM_INF"]) )
 
 mean_log10P <- tibble(FTO_SNP = c(F,T,F),
-                      mean_log10P = c(mean(-log10( (LMM_PXS_T2D_FTO%>%filter(BP < min(FTO_SNPs$pos)))$P_BOLT_LMM_INF)),
-                                      mean(-log10( (LMM_PXS_T2D_FTO%>%filter( between(BP, min(FTO_SNPs$pos), max(FTO_SNPs$pos)) ))$P_BOLT_LMM_INF)),
-                                      mean(-log10( (LMM_PXS_T2D_FTO%>%filter(BP > max(FTO_SNPs$pos)))$P_BOLT_LMM_INF))
+                      mean_log10P = c(mean(-log10( (LMM_BRS_FTO%>%filter(BP < min(FTO_SNPs$pos)))$P_BOLT_LMM_INF)),
+                                      mean(-log10( (LMM_BRS_FTO%>%filter( between(BP, min(FTO_SNPs$pos), max(FTO_SNPs$pos)) ))$P_BOLT_LMM_INF)),
+                                      mean(-log10( (LMM_BRS_FTO%>%filter(BP > max(FTO_SNPs$pos)))$P_BOLT_LMM_INF))
                                       ),
                       BP_min = c(min(FTO_SNPs$pos)-window, min(FTO_SNPs$pos), max(FTO_SNPs$pos)+1),
                       BP_max = c(min(FTO_SNPs$pos)-1, max(FTO_SNPs$pos), max(FTO_SNPs$pos)+window)
                       )
 
-LMM_PXS_T2D_FTO %>% filter(FTO_SNP, P_BOLT_LMM_INF < 1E-5)
+LMM_BRS_FTO %>% filter(FTO_SNP, P_BOLT_LMM_INF < 1E-5)
 ## mini Manhattan plot ####
-gg <- ggplot(LMM_PXS_T2D_FTO, aes(x=BP, y = -log10(P_BOLT_LMM_INF))) +
+gg <- ggplot(LMM_BRS_FTO, aes(x=BP, y = -log10(P_BOLT_LMM_INF))) +
   geom_hline(yintercept = -log10(5E-8), color = "red") +
   geom_hline(yintercept = -log10(1E-5), color = "blue") +
   geom_point(aes(color = FTO_SNP), alpha = 0.5, size=1) +
@@ -95,12 +79,12 @@ gg <- ggplot(LMM_PXS_T2D_FTO, aes(x=BP, y = -log10(P_BOLT_LMM_INF))) +
   )
 gg
 dir_Manhattan <- paste0(dir_script,"../final_results/Manhattan_plots/")
-loc_out <- paste0(dir_Manhattan, "Zoomed_Manhattan_PXS_T2D")
+loc_out <- paste0(dir_Manhattan, "Zoomed_Manhattan_BRS")
 ggsave(paste0(loc_out,".png"), gg, width=180, height = 120, units="mm", dpi=900)
 
 # gvc ~ -log10(P) ####
-LMM_PXS_T2D_FTO <- LMM_PXS_T2D_FTO %>%
+LMM_BRS_FTO <- LMM_BRS_FTO %>%
   mutate( gvc = 2 * (BETA)^2 * (A1FREQ) * (1-A1FREQ) )
-ggplot(LMM_PXS_T2D_FTO, aes(x = gvc, y = -log10(P_BOLT_LMM_INF))) +
+ggplot(LMM_BRS_FTO, aes(x = gvc, y = -log10(P_BOLT_LMM_INF))) +
   geom_point() +
   geom_abline(slope=1)
